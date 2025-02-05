db5f56a7e6883738022ed622e9eb0737d3fd2d7c
sirenabailie <sirenabailie@gmail.com>
2025-02-04 20:29:27 -0600

    update ticket

--- Program.cs
+++ Program.cs
@@ -151,4 +151,21 @@ app.MapDelete("/servicetickets/{id}", (int id) =>
     return Results.NoContent(); // NoContent tells us deletion was successful 
 });
 
+app.MapPut("/servicetickets/{id}", (int id, ServiceTicket serviceTicket) =>
+{
+    ServiceTicket ticketToUpdate = serviceTickets.FirstOrDefault(st => st.Id == id);
+    int ticketIndex = serviceTickets.IndexOf(ticketToUpdate);
+    if (ticketToUpdate == null)
+    {
+        return Results.NotFound();
+    }
+    //the id in the request route doesn't match the id from the ticket in the request body. That's a bad request!
+    if (id != serviceTicket.Id)
+    {
+        return Results.BadRequest();
+    }
+    serviceTickets[ticketIndex] = serviceTicket;
+    return Results.Ok();
+});
+
 app.Run();
